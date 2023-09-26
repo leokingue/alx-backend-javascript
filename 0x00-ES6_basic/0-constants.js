aebf0088e87424ebd31345d25c76927ea582ab2d
leokingue <ebangkingue@icloud.com>
2023-09-26 23:30:39 +0200

    Create the First Project on Backend JavaScript

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,16 @@
+const task = 'I prefer const when I can.';
+
+export function taskFirst() {
+    return task;
+}
+
+export function getLast() {
+    return ' is okay';
+}
+
+let combination = 'But sometimes let';
+export function taskNext() {
+    combination += getLast();
+
+    return combination
+}
