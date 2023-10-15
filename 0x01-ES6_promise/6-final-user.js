10bc41beec7207e0902e3cc0feb7c9f7e0046fab
leokingue <ebangkingue@icloud.com>
2023-10-15 22:33:04 +0200

    update the task6

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,11 @@
+import { signUser } from './4-user-promise';
+import { uploadPhoto } from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  Promise.allSettled([signUser(firstName, lastName), uploadPhoto(fileName)]).then((results) => [
+    {
+      status: results[0],
+      value: results[0].status === 'fulfilled' ? results[0].value : results[0].reason,
+    },
+  ]);
+}
