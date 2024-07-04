dc06a7c16dc3966d3c8807493c348f7d58b94d92
leokingue <ebangkingue@icloud.com>
2024-07-04 23:53:36 +0200

    Update the task3

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,3 @@
 export default function getStudentIdsSum(students) {
-    if (students instanceof Array) {
-        return students.reduce((sum, student) => sum + student.id, 0);
-    }
-}
+    return students.reduce((acc, c) => acc + c.id, 0);
+  }
